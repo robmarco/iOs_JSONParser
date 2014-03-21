40b5a800f2f8a5cfc4de660a7c59b41827265ef1
Roberto Marco <robmarco@gmail.com>
2014-03-21 02:11:41 +0100

    First commit JSONParserRM

---
+++ JSONParserRM/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JSONParserRM
+//
+//  Created by Roberto Marco on 21/03/14.
+//  Copyright (c) 2014 Roberto Marco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JSONParserRM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JSONParserRM
+//
+//  Created by Roberto Marco on 21/03/14.
+//  Copyright (c) 2014 Roberto Marco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
